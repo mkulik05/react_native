2dfd801ecd098c0ccbe016b5766f1180a78c2b95
epamekids <40541820+epamekids@users.noreply.github.com>
2019-01-02 19:06:15 +0300

    123

--- App.js
+++ App.js
@@ -1,63 +1,53 @@
-import React, { Component } from 'react';
-import { Platform, Text, View, StyleSheet,Button} from 'react-native';
-import { Constants, Location, Permissions } from 'expo';
+import React from 'react';
+import {Text}  from 'react-native';
+import { WebBrowser,Constants, Location, Permissions,MapView} from 'expo';
+import { MonoText } from '../components/StyledText';
+import * as firebase from 'firebase'
 
-export default class App extends Component {
-  state = {
-    location: null,
-    errorMessage: null,
-  };
+export default class HomeScreen extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+        location: null,
+        errorMessage: null,
+        region:null,
+      };
+      var config = {
+   apiKey: "AIzaSyDxqDaTcAUR3R6fZwI7PSz5H1yGhVnHHH4",
+   authDomain: "location-72fca.firebaseapp.com",
+   databaseURL: "https://location-72fca.firebaseio.com",
+   projectId: "location-72fca",
+   storageBucket: "location-72fca.appspot.com",
+   messagingSenderId: "440309375391"
+ };
+ firebase.initializeApp(config);
 
-  componentWillMount() {
-    if (Platform.OS === 'android' && !Constants.isDevice) {// если платформа android и приложение работает не в симуляторе
-      this.setState({
-        errorMessage: 'Oops, this will not work on Sketch in an Android emulator. Try it on your device!',
-      });
-    } else {
-      this._getLocationAsync(); //вызов функции
-    }
   }
 
-  _getLocationAsync = async () => {
-    let { status } = await Permissions.askAsync(Permissions.LOCATION); // Определяет, предоставлено ли вашему приложению доступ к предоставленному типу разрешения.
-    if (status !== 'granted') { // если доступа не получено
-      this.setState({
-      //  errorMessage: 'Permission to access location was denied',
-        });
-    }
+    _getLocationAsync = async () => {
+      let { status } = await Permissions.askAsync(Permissions.LOCATION); // Определяет, предоставлено ли вашему приложению доступ к предоставленному типу разрешения.
+      if (status !== 'granted') { // если доступа не получено
+        this.setState({
+          errorMessage: 'Permission to access location was denied',
+          });
+      }
 
-    let location = await Location.getCurrentPositionAsync({}); // если getCurrentPositionAsync выполнилось до конца
-    this.setState({ location:location });
-  };
+      let location = await Location.getCurrentPositionAsync({}); // если getCurrentPositionAsync выполнилось до конца
+
+      firebase.database().ref('location/').set({
+    loc: this.location
+  });
+      //alert(location.coords.longitude)
+        //}, 3000);
+        this.setState({
+          location:location,
+          });
+    };
 
   render() {
-    let text = 'please wait 5 seconds(or more)';
-      if (this.state.errorMessage) {
-        text = this.state.errorMessage;
-      } else if (this.state.location) {
-        text = JSON.stringify(this.state.location.coords); //переделывает объект в строку
-      } // this.state.location - объект
-
-
-    return (
-      <View style={styles.container}>
-        <Text style={styles.paragraph}>{text}</Text>
-      </View>
-    );
+      return(<Text>ok</Text>)
+    } else {
+      return(<Text>Waiting..</Text>)
+    }
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop: Constants.statusBarHeight,//Высота строки состояния по умолчанию для устройства. Не влияет на изменения, когда используется отслеживание местоположения или активен телефонный звонок.
-    backgroundColor: '#ecf0f1',
-  },
-  paragraph: {
-    margin: 24,
-    fontSize: 18,
-    textAlign: 'center',
-  },
-});
